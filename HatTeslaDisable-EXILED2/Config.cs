cbe65cfc37cc232b887d0e5fedce96552285ae6e
boogaeye <bradleigh.christensen@makt3k.com>
2020-09-14 16:18:40 -0700

    Add project files.

---
+++ HatTeslaDisable-EXILED2/Config.cs
@@ -0,0 +1,14 @@
+﻿using Exiled.API.Interfaces;
+using System.ComponentModel;
+
+namespace HatTeslaDisable_EXILED2
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        [Description("sets if the hat has to be held(and character not visable) to disable tesla gates for that person")]
+        public bool teslaHatHold { get; set; } = false;
+        [Description("sets if the person is invisable it doesnt allow damage to hurt the person while not visable")]
+        public bool hatGodMode { get; set; } = false;
+    }
+}
---
+++ HatTeslaDisable-EXILED2/HatTeslaDisable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+using Exiled.API.Enums;
+
+namespace HatTeslaDisable_EXILED2
+{
+    public class HatTeslaDisable : Plugin<Config>
+    {
+        private static readonly Lazy<HatTeslaDisable> LazyInstance = new Lazy<HatTeslaDisable>(valueFactory: () => new HatTeslaDisable());
+        static public HatTeslaDisable instance => LazyInstance.Value;
+        public override PluginPriority Priority { get; } = PluginPriority.Medium;
+        private HatTeslaDisable()
+        {
+
+        }
+        public override void OnEnabled()
+        {
+            RegisterEvents();
+        }
+
+        private void RegisterEvents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnDisabled()
+        {
+            UnregisterEvents();
+        }
+
+        private void UnregisterEvents()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
